1ba46d520657a1355dce0244f3b6b9b801e4256c
rashid <hakimrashid207@gmail.com>
2023-06-05 12:55:51 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send("working Fine")
+})
+
+app.listen(3000,()=>{
+    console.log('server is listening on port 3000')
+})
